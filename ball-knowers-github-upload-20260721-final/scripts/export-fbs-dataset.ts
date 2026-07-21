987f5aa810531f0ad2fd4566da0ba13aa0b70470
richmondred <richmondreddeer@gmail.com>
2026-07-21 22:16:25 +0100

    Delete ball-knowers-github-upload-20260721-final directory

--- ball-knowers-github-upload-20260721-final/scripts/export-fbs-dataset.ts
+++
@@ -1,8 +0,0 @@
-import { mkdirSync, writeFileSync } from "fs";
-import { dirname, resolve } from "path";
-import { fbs2026Dataset } from "@/data/fbs/fbs-2026.1";
-
-const output = resolve(process.argv[2] ?? "outputs/fbs-2026.1.json");
-mkdirSync(dirname(output), { recursive: true });
-writeFileSync(output, `${JSON.stringify(fbs2026Dataset, null, 2)}\n`, "utf8");
-console.log(`Exported ${fbs2026Dataset.datasetVersion} to ${output}`);
